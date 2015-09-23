camunigeography/muscat-conversion

commit 4c083d7baeae1c171884cd1f7e1dad59d0beed38

Generate 008: In lookupValue, handle fallback by adding a virtual value in the table, not converting the incoming value

generate008.php
@@ -808,9 +808,6 @@ class generate008
 	# Generalised lookup table function
 	private function lookupValue ($table, $value, $field, $ifEmptyUseValueFor, $caseSensitiveComparison = true, $stripBrackets = false)
 	{
-		# If the supplied value is empty, and a fallback is defined, treat the value as the fallback, which will then be looked up
-		if (!$value) {$value = $ifEmptyUseValueFor;}
-		
 		# Get the data table
 		$lookupTable = file_get_contents ($this->muscatConversion->applicationRoot . '/tables/' . $table . '.tsv');
 		
@@ -818,6 +815,11 @@ class generate008
 		$lookupTable = trim ($lookupTable);
 		require_once ('csv.php');
 		$lookupTableRaw = csv::tsvToArray ($lookupTable, $firstColumnIsId = true);
+		
+		# Define the fallback value in case that is needed
+		if ($fallbackKey) {
+			$lookupTableRaw[false] = $lookupTableRaw[$fallbackKey];		// Boolean false is used because the value comes from an xPathValue() lookup, which will be false if no match
+		}
 		
 		# Perform conversions on the key names
 		$lookupTable = array ();

commit d1e88cb48b0dc49b4fd2244865c33d659cfcd1ef

008 field: Add further fix for fallback, removing regression in 4c083d7baeae1c171884cd1f7e1dad59d0beed38

generate008.php
@@ -845,9 +845,10 @@ class generate008
 		$lookupTableRaw = csv::tsvToArray ($lookupTable, $firstColumnIsId = true);
 		
 		# Define the fallback value in case that is needed
-		if ($fallbackKey) {
-			$lookupTableRaw[false] = $lookupTableRaw[$fallbackKey];		// Boolean false is used because the value comes from an xPathValue() lookup, which will be false if no match
+		if (!isSet ($lookupTableRaw[''])) {
+			$lookupTableRaw['']		= $lookupTableRaw[$fallbackKey];
 		}
+		$lookupTableRaw[false]	= $lookupTableRaw[$fallbackKey];	// Boolean false also needs to be defined because no-match value from an xPathValue() lookup will be false
 		
 		# Perform conversions on the key names
 		$lookupTable = array ();