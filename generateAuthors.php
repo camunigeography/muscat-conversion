e0af1c5cf199b6488278b1b88eff324ff54de094
mvl22 <martin@lucas-smith.co.uk>
2015-10-29 18:40:59 +0000

    In generateAuthors, avoid use of parent:: to simplify hierarchy, as *ser / *art/*in check means possible parents are already implied

--- generateAuthors.php
+++ generateAuthors.php
@@ -74,8 +74,8 @@ class generateAuthors
 		
 		# Look at the first or only *doc/*ag/*a OR *art/*ag/*a
 		# Does the *a contain a *n2?
-		$n2 = $this->muscatConversion->xPathValue ($this->xml, '(//ag[parent::doc|parent::art]/a)[1]/n2', false);
-		$n1 = $this->muscatConversion->xPathValue ($this->xml, '(//ag[parent::doc|parent::art]/a)[1]/n1', false);
+		$n2 = $this->muscatConversion->xPathValue ($this->xml, '(/root/*/ag/a)[1]/n2', false);
+		$n1 = $this->muscatConversion->xPathValue ($this->xml, '(/root/*/ag/a)[1]/n1', false);
 		if (strlen ($n2)) {
 			
 			# Add to 100 field: 1# ‡a <*a/*n1>,
