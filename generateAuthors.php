d4d7fa404a7123d734c75f57babba066cff04622
mvl22 <martin@lucas-smith.co.uk>
2015-10-29 17:20:59 +0000

    700 field: Document the process flow, prior to coding it up

--- generateAuthors.php
+++ generateAuthors.php
@@ -43,6 +43,12 @@ class generateAuthors
 	
 	
 	# Generate 100
+	/*
+	 * This is basically the first author.
+	 * It may end up switching to 110/111 instead.
+	 * Everyone else involved in the production ends put in 7xx fields.
+	 *
+	 */
 	public function generate100 ()
 	{
 		# Start the value
@@ -102,7 +108,26 @@ class generateAuthors
 	}
 	
 	
-	# Generate 700
+	# Generate 700; see: http://www.loc.gov/marc/bibliographic/bd700.html
+	/*
+	 * This is basically all the people involved in the book except the first author, which if present is covered in 100/110/111.
+	 * It includes people in the analytic (child) records, but limited to the first of them for each such child record
+	 * This creates multiple 700 lines, the lines being created as the outcome of the loop below
+	 * Each "contributor block" referenced below refers to the author components, which are basically the 'classify' functions elsewhere in this class
+	 * 
+	 * - Checks there is *doc/*ag or *art/*ag (i.e. *ser records will be ignored)
+	 * - Loops through each *ag
+	 * - Within each *ag, for each *a and *al add the contributor block
+	 * - In the case of each *ag/*al, the "*al Detail" block (and ", ‡g (alternative name)", once only) is added
+	 * - Loop through each *e
+	 * - Within each *e, for each *n add the contributor block
+	 * - In the case of each *e/*n, *role, with Relator Term lookup substitution, is incorporated
+	 * - When considering the *e/*n, there is a guard clause to skip cases of 'the author' as the 100 field would have already pulled in that person (e.g. the 100 field could create "<name> $eIllustrator" indicating the author <name> is also the illustrator)
+	 * - Checks for a *ke which is a flag indicating that there are analytic (child) records, e.g. as present in /records/7463/
+	 * - Looks up the records whose *kg matches, e.g. /records/9375/ has *kg=7463, so this indicates that 9375 (which will be an *art) is a child of 7463
+	 * - For each *kg's *art (i.e. child *art record): take the first *art/*ag/*a/ (only the first) in that record within the *ag block, i.e. /records/9375/ /art/ag/a "contributor block", and also add the title of the (i.e. *art/*tg/*t); the second indicator is set to '2' to indicate that this 700 line is an 'Analytical entry'
+	 * - Every 700 has a fixed string ", ‡5 UkCU-P" at the end (representing the Institution to which field applies)
+	 */
 	public function generate700 ()
 	{
 		// #!# TODO
