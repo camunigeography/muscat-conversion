cb909af7d7988ba63daf01a85ac7a89205b20b26
mvl22 <martin@lucas-smith.co.uk>
2015-11-10 12:45:42 +0000

    Fix off-by-one space error after LDR which causes .mrk to be incorrect

--- muscatConversion.php
+++ muscatConversion.php
@@ -3305,8 +3305,8 @@ class muscatConversion extends frontControllerApplication
 		exec ("sed -i '/^008 /s/#/\\\\/g' {$filenameMarcExchange}");												// Replace all instances of a # marker in the 008 field with \
 		exec ("perl -pi -e 's" . '/^([0-9]{3}) #(.) (.+)$/' . '\1 \\\\\2 \3' . "/' {$filenameMarcExchange}");		// Replace # marker in position 1 with \
 		exec ("perl -pi -e 's" . '/^([0-9]{3}) (.)# (.+)$/' . '\1 \2\\\\ \3' . "/' {$filenameMarcExchange}");		// Replace # marker in position 2 with \
-		exec ("perl -pi -e 's" . '/^([0-9]{3}) (.+)$/' . '\1  \2' . "/' {$filenameMarcExchange}");				// Add double-space
-		exec ("perl -pi -e 's" . '/^([0-9]{3})  (.)(.) (.+)$/' . '\1  \2\3\4' . "/' {$filenameMarcExchange}");	// Remove space after marker
+		exec ("perl -pi -e 's" . '/^([0-9]{3}|LDR) (.+)$/' . '\1  \2' . "/' {$filenameMarcExchange}");				// Add double-space after LDR and each field number
+		exec ("perl -pi -e 's" . '/^([0-9]{3})  (.)(.) (.+)$/' . '\1  \2\3\4' . "/' {$filenameMarcExchange}");		// Remove space after first and second indicators
 		exec ("perl -pi -e 's" . '/^(.+)$/' . '=\1' . "/' {$filenameMarcExchange}");								// Add = at start of each line
 		
 		# Create a binary version
