7baad881f0313c6199c8de0946fdbf5fa36a21b1
mvl22 <martin@lucas-smith.co.uk>
2015-11-17 13:51:26 +0000

    Chunk the subscripts/superscripts replacements, as the ~380 replacements were causing MySQL error 1436: Thread stack overrun

--- muscatConversion.php
+++ muscatConversion.php
@@ -2505,7 +2505,10 @@ class muscatConversion extends frontControllerApplication
 		
 		# Subscripts and superscripts, e.g. "H{2}SO{4} will print out as H2SO4 with both 2 and 4 as subscripts"
 		$subscriptsSuperscriptsReplacements = $this->getSubscriptsSuperscriptsReplacementsDefinition ();
-		$queries[] = "UPDATE catalogue_processed SET value = " . $this->databaseConnection->replaceSql ($subscriptsSuperscriptsReplacements, 'value', "'") . ';';
+		$subscriptsSuperscriptsReplacementsChunks = array_chunk ($subscriptsSuperscriptsReplacements, $chunksOf = 25, true);
+		foreach ($subscriptsSuperscriptsReplacementsChunks as $subscriptsSuperscriptsReplacementsChunk) {
+			$queries[] = "UPDATE catalogue_processed SET value = " . $this->databaseConnection->replaceSql ($subscriptsSuperscriptsReplacementsChunk, 'value', "'") . ';';
+		}
 		
 		# Greek characters; see also report_specialcharscase which enables the librarians to normalise \gGamMA to \gGamma
 		# Assumes this catalogue rule has been eliminated: "When '\g' is followed by a word, the case of the first letter is significant. The remaining letters can be in either upper or lower case however. Thus '\gGamma' is a capital gamma, and the forms '\gGAMMA', '\gGAmma' etc. will also represent capital gamma."
