camunigeography/muscat-conversion

commit 272b0032f5abe3bf555011a65f323a0b313248db

Replicate structure more consistently when doing vertical repeatability split cloning

muscatConversion.php
@@ -3680,8 +3680,8 @@ class muscatConversion extends frontControllerApplication
 			
 			# Initialise arrays to ensure attributes for each line are present
 			$datastructure[$lineNumber]['controlCharacters'] = array ();
-			$datastructure[$lineNumber]['xpathReplacements'] = array ();
 			$datastructure[$lineNumber]['macros'] = array ();
+			$datastructure[$lineNumber]['xpathReplacements'] = array ();
 			
 			# Validate and extract the syntax
 			if (!preg_match ('/^([A-Z]*)\s+([0-9|LDR]{3} .{3}.+)$/', $line, $matches)) {
@@ -3898,11 +3898,10 @@ class muscatConversion extends frontControllerApplication
 					# Assign the new key (original key, plus the subvalue index)
 					$newLineNumber = "{$lineNumber}_{$index}";
 					
-					# Clone the line to a new line, and do the same for control characters
-					$datastructure[$newLineNumber]['line'] = $line['line'];
-					$datastructure[$newLineNumber]['controlCharacters'] = $line['controlCharacters'];
+					# Clone the line, as-is
+					$datastructure[$newLineNumber] = $line;
 					
-					# Move each subfield value so that it is indexed by line
+					# Overwrite the subfield value, so it contains only this subfield value, not the whole array of values
 					$datastructure[$newLineNumber]['xpathReplacements'][$find] = $value;
 				}
 			}

commit 17d4a984d82352f2db3e0fbfeb968560caeffe6f

Record processor: Fix painful regression in 272b0032f5abe3bf555011a65f323a0b313248db and 59916d2fe18add8a39c10797326046956d5ee8f3

muscatConversion.php
@@ -4392,19 +4392,20 @@ class muscatConversion extends frontControllerApplication
 				continue;
 			}
 			
-			# Split each original line then discard the original
-			foreach ($line['xpathReplacements'] as $macroBlock => $xpathReplacementSpec) {
-				$replacementValues = $xpathReplacementSpec['replacement'];
-				foreach ($replacementValues as $index => $value) {
-					
-					# Assign the new key (original key, plus the subvalue index)
-					$newLineNumber = "{$lineNumber}_{$index}";
-					
-					# Clone the line, as-is
-					$datastructure[$newLineNumber] = $line;
-					
-					# Overwrite the subfield value, so it contains only this subfield value, not the whole array of values
-					$datastructure[$newLineNumber]['xpathReplacements'][$macroBlock]['replacement'] = $value;
+			# Determine the number of line expansions (which the above check should ensure is consistent between each of the counts)
+			$numberOfLineExpansions = application::array_first_value ($counts);		// Take the first count only
+			
+			# Clone the line, one for each subvalue, as-is, assigning a new key (original key, plus the subvalue index)
+			for ($subLine = 0; $subLine < $numberOfLineExpansions; $subLine++) {
+				$newLineId = "{$lineNumber}_{$subLine}";	// e.g. 17_0, 17_1 if there are two line expansion
+				$datastructure[$newLineId] = $line;
+			}
+			
+			# Overwrite the subfield value within the structure, so it contains only this subfield value, not the whole array of values
+			for ($subLine = 0; $subLine < $numberOfLineExpansions; $subLine++) {
+				$newLineId = "{$lineNumber}_{$subLine}";
+				foreach ($line['xpathReplacements'] as $macroBlock => $xpathReplacementSpec) {
+					$datastructure[$newLineId]['xpathReplacements'][$macroBlock]['replacement'] = $xpathReplacementSpec['replacement'][$subLine];
 				}
 			}
 		}
@@ -4434,7 +4435,6 @@ class muscatConversion extends frontControllerApplication
 					$replacementValue = $xpathReplacementSpec['replacement'];
 					
 					# Determine if there is content
-					#!# "strlen() expects parameter 1 to be string, array given" in /records/2326/
 					$blockHasValue = strlen ($replacementValue);
 					
 					# Register replacements