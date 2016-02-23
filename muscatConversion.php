camunigeography/muscat-conversion

commit fefc9dee1fed18d313faae25b488c32d2d8a4179

Add report for records containing more than one top-level *t field

muscatConversion.php
@@ -66,6 +66,7 @@ class muscatConversion extends frontControllerApplication
 		'multiplecopies_info' => 'records where there appear to be multiple copies, in notes field',
 		'multiplein_problem' => 'records containing more than one *in field',
 		'multiplej_problem' => 'records containing more than one *j field',
+		'multipletopt_problem' => 'records containing more than one top-level *t field',
 		'multipletoptt_problem' => 'records containing more than one top-level *tt field',
 		'invaliddatestring_problem' => 'records with an invalid date string (though some are valid)',
 		'serlocloc_problem' => '*ser records with two or more locations (though some are valid)',
@@ -8320,6 +8321,23 @@ class muscatConversion extends frontControllerApplication
 			FROM fieldsindex
 			WHERE ((LENGTH(fieldslist)-LENGTH(REPLACE(fieldslist,'@j@',''))) / LENGTH('@j@')) > 1
 			";
+		
+		# Return the query
+		return $query;
+	}
+	
+	
+	# Records containing more than one top-level *t field
+	private function report_multipletopt ()
+	{
+		# Define the query
+		$query = "
+			SELECT
+				'multipletopt' AS report,
+				id AS recordId
+			FROM catalogue_xml
+			WHERE EXTRACTVALUE(xml, 'count(/*/tg/t)') > 1	/* This will catch both /*/tg/t[2] and /*/tg[2]/t
+		";
 		
 		# Return the query
 		return $query;

commit a1970894a9bd5a3d560c3e349215971fdfbe1866

Fix unterminated comment bug in fefc9dee1fed18d313faae25b488c32d2d8a4179 which caused import to break

muscatConversion.php
@@ -1089,7 +1089,7 @@ class muscatConversion extends frontControllerApplication
 				<n1 />
 				<n2 />
 				<nd />
-			</a>	
+			</a>
 			<ad /><!-- authorial detail (usually 'and others' 'ed.' 'eds.') -->
 			<al><!-- alternative spelling of transliterated name or alternative name -->
 				<n1 />
@@ -8564,7 +8564,7 @@ class muscatConversion extends frontControllerApplication
 				'multipletopt' AS report,
 				id AS recordId
 			FROM catalogue_xml
-			WHERE EXTRACTVALUE(xml, 'count(/*/tg/t)') > 1	/* This will catch both /*/tg/t[2] and /*/tg[2]/t
+			WHERE EXTRACTVALUE(xml, 'count(/*/tg/t)') > 1	-- This will catch both /*/tg/t[2] and /*/tg[2]/t
 		";
 		
 		# Return the query