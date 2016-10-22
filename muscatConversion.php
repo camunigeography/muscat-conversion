0f123eab4b02d51b217e161c75bba1bf7b5bb036
mvl22 <martin@lucas-smith.co.uk>
2016-10-22 21:48:28 +0100

    Add support for marking reports as post-migration tasks

--- muscatConversion.php
+++ muscatConversion.php
@@ -390,7 +390,7 @@ class muscatConversion extends frontControllerApplication
 		# Loop through each report, detecting whether each report is informational, and rewriting the name
 		$reports = array ();	// Array of report key names without flag appended
 		foreach ($reportsRaw as $key => $value) {
-			if (preg_match ('/^(.+)_(info|problem|problemok)$/', $key, $matches)) {
+			if (preg_match ('/^(.+)_(info|postmigration|problem|problemok)$/', $key, $matches)) {
 				$key = $matches[1];
 				$reportStatuses[$key] = $matches[2];
 				$reports[$key] = $value;	// Register under new name
