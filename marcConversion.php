camunigeography/muscat-conversion

commit 31a6a4907a6b443348db2c2e542eaae8d9e630e3

Fix other names data processing to avoid re-requesting data on future runs

muscatConversion.php
@@ -4855,8 +4855,7 @@ class muscatConversion extends frontControllerApplication
 			UPDATE transliterations
 			INNER JOIN othernames ON transliterations.title = othernames.surname
 			SET inNameAuthorityList = othernames.results
-			WHERE
-				    transliterations.field IN('" . implode ("', '", $this->transliterationNameMatchingFields) . "')
+			WHERE transliterations.field IN('" . implode ("', '", $this->transliterationNameMatchingFields) . "')
 		;";
 		$this->databaseConnection->query ($query);
 		
@@ -4869,20 +4868,21 @@ class muscatConversion extends frontControllerApplication
 	private function obtainSaveOtherNamesData ($file)
 	{
 		# Get list of names needing population
-		$limit = 100;
 		$query = "SELECT
 			DISTINCT title
 			FROM transliterations
 			WHERE
-				    field IN('n1')
-				AND inNameAuthorityList = 0
+				    field IN('" . implode ("', '", $this->transliterationNameMatchingFields) . "')
+				AND inNameAuthorityList IS NULL							-- i.e. hasn't yet been allocated any value
+				AND title NOT IN(
+					SELECT DISTINCT surname AS title FROM othernames	-- 5965 values, so IN() performance will not be completely terrible
+				)
 			ORDER BY id
-			LIMIT {$limit}
 		;";
 		$names = $this->databaseConnection->getPairs ($query);
 		
 		# Set user agent
-		ini_set ('user_agent', 'Scott Polar Research Institute - Muscat catalogue conversion project. Using Wikipedia to obtain counts for 6,000 Russian authors.');
+		ini_set ('user_agent', 'Scott Polar Research Institute - Muscat catalogue conversion project. Using Russian Wikipedia to obtain hit counts for c. 6,000 Russian authors.');
 		
 		# Work through each name
 		$results = array ();

commit ada175400cc691e7b2106e8b48a1732e98d94c7e

Resolve circular dependency bug introduced in 31a6a4907a6b443348db2c2e542eaae8d9e630e3 in populateOtherNames where obtainSaveOtherNamesData relied on data from previous import run

marcConversion.php
@@ -91,7 +91,7 @@ class marcConversion
 		
 		# Do a check to report any case of an invalid subfield indicator
 		if (preg_match_all ("/{$this->doubleDagger}[^a-z0-9]/u", $record, $matches)) {
-			$errorString .= "Invalid " . (count ($matches[0]) == 1 ? 'subfield' : 'subfields') . " (" . implode (', ', $matches[0]) . ") detected in record <a href=\"{$this->baseUrl}/records/{$this->recordId}/\">#{$this->recordId}</a>.";
+			$errorString .= 'Invalid ' . (count ($matches[0]) == 1 ? 'subfield' : 'subfields') . " (" . implode (', ', $matches[0]) . ") detected in record <a href=\"{$this->baseUrl}/records/{$this->recordId}/\">#{$this->recordId}</a>.";
 			// Leave the record visible rather than return false
 		}
 		

muscatConversion.php
@@ -4820,9 +4820,32 @@ class muscatConversion extends frontControllerApplication
 		# Define the other names data file
 		$file = $this->applicationRoot . '/tables/othernames.tsv';
 		
-		# Build up this data up over time
+		# Initialise the other names data table with existing data
+		$this->buildOtherNamesTable ($file);
+		
+		# Add any new values to the data file
 		$this->obtainSaveOtherNamesData ($file);
 		
+		# Re-initialise the other names data table with existing data
+		$this->buildOtherNamesTable ($file);
+		
+		# Perform matches
+		$query = "
+			UPDATE transliterations
+			INNER JOIN othernames ON transliterations.title = othernames.surname
+			SET inNameAuthorityList = othernames.results
+			WHERE transliterations.field IN('" . implode ("', '", $this->transliterationNameMatchingFields) . "')
+		;";
+		$this->databaseConnection->query ($query);
+		
+		# Confirm success
+		return true;
+	}
+	
+	
+	# Function to insert other names data
+	private function buildOtherNamesTable ($file)
+	{
 		# Read in the TSV file
 		$tsv = file_get_contents ($file);
 		
@@ -4849,18 +4872,6 @@ class muscatConversion extends frontControllerApplication
 			$error = 'Error inserting other names data';
 			return false;
 		}
-		
-		# Perform matches
-		$query = "
-			UPDATE transliterations
-			INNER JOIN othernames ON transliterations.title = othernames.surname
-			SET inNameAuthorityList = othernames.results
-			WHERE transliterations.field IN('" . implode ("', '", $this->transliterationNameMatchingFields) . "')
-		;";
-		$this->databaseConnection->query ($query);
-		
-		# Confirm success
-		return true;
 	}
 	
 	