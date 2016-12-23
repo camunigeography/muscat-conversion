5a9bfae3fe06a7652d12d1feb698720a2abe397c
mvl22 <martin@lucas-smith.co.uk>
2016-12-23 16:19:16 +0000

    Clarify and tighten HTML tag handling in transliterated protected string routine

--- transliteration.php
+++ transliteration.php
@@ -243,8 +243,8 @@ class transliteration
 		preg_match_all ('|(<em>.+</em>)|uU', $string, $italicisedNameMatches);		// Uses /U ungreedy, to avoid "a <em>b</em> c <em>d</em> e" becoming "a  e" (test #46)
 		$replacements = array_merge ($replacements, $italicisedNameMatches[1]);
 		
-		# Add in HTML tags for protection; NB in theory all <em> and </em> tags will have been swallowed already so in practice these are not necessary here
-		$tags = array ('<em>', '</em>', '<sub>', '</sub>', '<sup>', '</sup>', );
+		# Protect HTML tags, protecting the tag string itself, not its contents
+		$tags = array ('<sub>', '</sub>', '<sup>', '</sup>', );
 		$replacements = array_merge ($replacements, $tags);
 		
 		# Protect known strings to protect from transliteration (Latin abbreviations, Order names, Roman numeral pattern regexps)
