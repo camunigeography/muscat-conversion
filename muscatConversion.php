685f0e0f503a8336c50182e4d98003c1dd516906
mvl22 <martin@lucas-smith.co.uk>
2017-10-29 13:02:22 +0000

    Update timings following successful import

--- muscatConversion.php
+++ muscatConversion.php
@@ -2215,14 +2215,14 @@ class muscatConversion extends frontControllerApplication
 		# Define the import types
 		$importTypes = array (
 			'full'					=> 'FULL import (c. 10.8 hours)',
-			'xml'					=> 'Regenerate XML only (c. 6 minutes)',
-			'marc'					=> 'Regenerate MARC only (c. 1.25 hours)',
-			'external'				=> 'Regenerate external Voyager records only (c. ? minutes)',
+			'xml'					=> 'Regenerate XML only (c. 6 minutes / 6.75 hours fresh)',
+			'marc'					=> 'Regenerate MARC only (c. 1.1 hours)',
+			'external'				=> 'Regenerate external Voyager records only (c. 5 seconds)',
 			'outputstatus'			=> 'Regenerate output status only (c. 15 seconds)',
-			'exports'				=> 'Regenerate MARC export files and Bibcheck report (c. 30 minutes)',
+			'exports'				=> 'Regenerate MARC export files and Bibcheck report (c. 15 minutes)',
 			'tests'					=> 'Run automated tests',
 			'reports'				=> 'Regenerate reports only (c. 7 minutes)',
-			'listings'				=> 'Regenerate listings reports only (c. 45 minutes)',
+			'listings'				=> 'Regenerate listings reports only (c. 30 minutes)',
 			'searchtables'			=> 'Regenerate search tables (c. 20 seconds)',
 		);
 		
@@ -2759,6 +2759,9 @@ class muscatConversion extends frontControllerApplication
 	#   Dependencies: fieldsindex, catalogue_xml, catalogue_marc
 	private function createSearchTables ()
 	{
+		# Log start
+		$this->logger ('Starting ' . __METHOD__);
+		
 		# Clone the fieldsindex table, including indexes, first dropping any existing table from a previous import; see: http://stackoverflow.com/a/3280042/180733
 		$this->databaseConnection->query ('DROP TABLE IF EXISTS searchindex;');
 		$this->databaseConnection->query ('CREATE TABLE searchindex LIKE fieldsindex;');
