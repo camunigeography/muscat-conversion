218e7e1be0fe132631eb558d7b183ed4432a8629
mvl22 <martin@lucas-smith.co.uk>
2017-12-09 16:56:43 +0000

    Fix failing test for dollar in 541 due to deleted record

--- createMarcExport.php
+++ createMarcExport.php
@@ -204,7 +204,7 @@ class createMarcExport
 			'500: Subfield _- is not allowed.',	// E.g. /records/138509/ has "24-3, $-24-4"
 			'500: Subfield _1 is not allowed.',	// E.g. /records/142058/ has "price: $195"
 			'520: Subfield _[1m,2t)] is not allowed.',	// E.g. /records/140044/ (test #223)
-			'541: Subfield _[0-9AUNC ] is not allowed.',	// E.g. /records/145475/ which has "AUS$ " (test #224); see example at: https://www.loc.gov/marc/bibliographic/bd541.html which confirms use of unescaped $
+			'541: Subfield _[0-9AUNC ] is not allowed.',	// E.g. /records/148863/ which has "AUS$ " (test #224); see example at: https://www.loc.gov/marc/bibliographic/bd541.html which confirms use of unescaped $
 			'541: Subfield _[0-9] is not repeatable.',	// The generate541 code definitely has no horizontal repeatability - this is Bibcheck being unable to distinguish e.g. $5 (money) from double-dagger5 (subfield), e.g. /records/9220/ (test #225)
 			'Record is post 1900 but contains local information \(541 or 561 fields\) - please check.',	// For 541; confirmed fine as we are setting $5, e.g. /records/9220/ (test #226)
 			'6XX: Unless the Literary form in the 008 is set to one of the fiction codes, there must be at least one 6XX field \(ignore if the work is a sacred text.\)',		// This arises because Bibcheck has a litcode check at line 602 but that assumes that the 008 is a "008 - Books" which is not always the case - see position_18_34__33 in generate008; see e-mail dated 30/Mar/2016 investigating this, and e-mail 31/Mar/2016 confirming the error is safe to suppress; e.g. /records/1061/ (test #227)
