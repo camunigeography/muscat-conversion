cae3abe7e9ac7984f60dff093eb5457c7a2b0bc3
mvl22 <martin@lucas-smith.co.uk>
2018-01-04 17:15:37 +0000

    300 ‡b Add support for explicit space-colon-space split point

--- marcConversion.php
+++ marcConversion.php
@@ -1145,14 +1145,24 @@ class marcConversion
 		$abbreviations = array ('col', 'diag', 'fig', 'ill', 'illus', 'port');
 		$pOrPt = preg_replace ('/' . "\b" . '(' . implode ('|', $abbreviations) . ')(s?)' . "\b" . '([^\.]|$)' . '/', '\1\2.\3', $pOrPt);
 		
-		# Next split by the keyword which acts as separating point between citation and an optional $b (i.e. is the start of an optional $b); e.g. /records/51787/ (test #328); first comma cannot be used reliably because the pagination list could be e.g. "3,5,97-100"; split is done for the first instance of a split word, e.g. /records/12780/ (test #535)
-		$splitWords = array ('col', 'diag', 'fig', 'figures', 'graph', 'ill', 'illus', 'map', 'port', 'table', );	// These may be pluralised, using the s? below; e.g. /records/1684/ (test #512)
+		# Split off the physical description; prefer explicit " : " marker where present; else use splitting point tokens
 		$pOrPt = trim ($pOrPt);
 		$physicalDescription = false;
-		$matches = preg_split ('/' . "\b" . '((?:' . implode ('|', $splitWords) . ')s?' . "\b.*$)" . '/', $pOrPt, 2, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY);		// Use of \b word boundary ensures not splitting at substrings, e.g. bibliography at 'graph': /records/54670/ (test #220); .* is used so that both appreviated types ("ill.", "diag.") and non-abbreviated types ("tables", "map") - see test for latter: /records/24489/ (test #630)
-		if (count ($matches) == 2) {
+		if (substr_count ($pOrPt, ' : ')) {
+			
+			# Explicit space-colon-space split point; e.g. "2 computer disks (45.5 min.; 54.5 min.) : sd." present in /records/199582/ (test #631)
+			$matches = explode (' : ', $pOrPt, 2);
 			$pOrPt = trim ($matches[0]);
 			$physicalDescription = trim ($matches[1]);
+		} else {
+			
+			# Next split by the keyword which acts as separating point between citation and an optional $b (i.e. is the start of an optional $b); e.g. /records/51787/ (test #328); first comma cannot be used reliably because the pagination list could be e.g. "3,5,97-100"; split is done for the first instance of a split word, e.g. /records/12780/ (test #535)
+			$splitWords = array ('col', 'diag', 'fig', 'figures', 'graph', 'ill', 'illus', 'map', 'port', 'table', );	// These may be pluralised, using the s? below; e.g. /records/1684/ (test #512)
+			$matches = preg_split ('/' . "\b" . '((?:' . implode ('|', $splitWords) . ')s?' . "\b.*$)" . '/', $pOrPt, 2, PREG_SPLIT_DELIM_CAPTURE|PREG_SPLIT_NO_EMPTY);		// Use of \b word boundary ensures not splitting at substrings, e.g. bibliography at 'graph': /records/54670/ (test #220); .* is used so that both appreviated types ("ill.", "diag.") and non-abbreviated types ("tables", "map") - see test for latter: /records/24489/ (test #630)
+			if (count ($matches) == 2) {
+				$pOrPt = trim ($matches[0]);
+				$physicalDescription = trim ($matches[1]);
+			}
 		}
 		
 		# At this point, $pOrPt represents the citation
