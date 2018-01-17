6cfe863409f587eaa73fcd119f4563487fc7a54e
mvl22 <martin@lucas-smith.co.uk>
2018-01-17 17:04:46 +0000

    Unwhitelist, and remove test for, cases of $ in 300, as costs should not be in *p/*pt

--- createMarcExport.php
+++ createMarcExport.php
@@ -200,7 +200,6 @@ class createMarcExport
 			'245: Subfield _[1|2|5] is not allowed.',	// E.g. /records/203691/ has $100 (test #218)
 			'245: Must have a subfield _a.',	// E.g. /records/174312/ has "‡a$50,000 an ounce!"
 			'300: In subfield _a, p should be followed by a full stop.',	// See 8e5f9354da83b6aa7a9e338e0ba7d48e1d1e0b60 - intended "p." is already implemented correctly; see /records/54670/ (test #219)
-			'300: Subfield _4 is not allowed.',	// /records/38727/ has $4.95 (test #222)
 			'500: Subfield _2 is not allowed.',	// E.g. /records/161883/ has $220 (test #558)
 			'500: Subfield _- is not allowed.',	// E.g. /records/138509/ has "24-3, $-24-4"
 			'500: Subfield _1 is not allowed.',	// E.g. /records/142058/ has "price: $195"
