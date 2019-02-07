47c99ebb313f53d95691122eeeed55cc02f969cd
mvl22 <martin@lucas-smith.co.uk>
2019-02-07 13:53:18 +0000

    Whitelist bogus Bibcheck error "250: Should end with a full stop." following change in adea4f30798d41ade39867408f7a2b14cd22cb7b

--- createMarcExport.php
+++ createMarcExport.php
@@ -239,6 +239,7 @@ class createMarcExport
 			'245: First word, een, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, ho, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, uno, may be an article, check 2nd indicator \(0\).',	// 1 case
+			'250: Should end with a full stop.'	// 250 dot-end is confirmed dot or punctuation
 			'300: In subfield _a, p should be followed by a full stop.',	// See 8e5f9354da83b6aa7a9e338e0ba7d48e1d1e0b60 - intended "p." is already implemented correctly; see /records/54670/ (test #219)
 			'300: In subfield _a there should be a space between the comma and the next set of digits.',	// E.g. /records/32362/
 			'300: In subfield _a there should be a space between the number and the type of unit - please check.',	// Only /records/164582/ and /records/203582/
