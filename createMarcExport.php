camunigeography/muscat-conversion

commit 47c99ebb313f53d95691122eeeed55cc02f969cd

Whitelist bogus Bibcheck error "250: Should end with a full stop." following change in adea4f30798d41ade39867408f7a2b14cd22cb7b

createMarcExport.php
@@ -239,6 +239,7 @@ class createMarcExport
 			'245: First word, een, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, ho, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, uno, may be an article, check 2nd indicator \(0\).',	// 1 case
+			'250: Should end with a full stop.'	// 250 dot-end is confirmed dot or punctuation
 			'300: In subfield _a, p should be followed by a full stop.',	// See 8e5f9354da83b6aa7a9e338e0ba7d48e1d1e0b60 - intended "p." is already implemented correctly; see /records/54670/ (test #219)
 			'300: In subfield _a there should be a space between the comma and the next set of digits.',	// E.g. /records/32362/
 			'300: In subfield _a there should be a space between the number and the type of unit - please check.',	// Only /records/164582/ and /records/203582/

commit 0cfde22af264eb4ecbd35b023a0d45c694c4056e

Fix syntax errors introduced in 47c99ebb313f53d95691122eeeed55cc02f969cd and 646d2e718edcbc3b31a673915aab666f6cc739ca

createMarcExport.php
@@ -239,7 +239,7 @@ class createMarcExport
 			'245: First word, een, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, ho, may be an article, check 2nd indicator \(0\).',	// 1 case
 			'245: First word, uno, may be an article, check 2nd indicator \(0\).',	// 1 case
-			'250: Should end with a full stop.'	// 250 dot-end is confirmed dot or punctuation
+			'250: Should end with a full stop.',	// 250 dot-end is confirmed dot or punctuation
 			'300: In subfield _a, p should be followed by a full stop.',	// See 8e5f9354da83b6aa7a9e338e0ba7d48e1d1e0b60 - intended "p." is already implemented correctly; see /records/54670/ (test #219)
 			'300: In subfield _a there should be a space between the comma and the next set of digits.',	// E.g. /records/32362/
 			'300: In subfield _a there should be a space between the number and the type of unit - please check.',	// Only /records/164582/ and /records/203582/
@@ -252,7 +252,7 @@ class createMarcExport
 			'533: Subfield _5 is not allowed.',	// E.g. /records/43953/ but this is clearly defined at https://www.loc.gov/marc/bibliographic/bd533.html
 			'541: Subfield _[0-9AUNC ] is not allowed.',	// E.g. /records/148863/ which has "AUS$ " (test #224); see example at: https://www.loc.gov/marc/bibliographic/bd541.html which confirms use of unescaped $
 			'541: Subfield _[0-9] is not repeatable.',	// The generate541 code definitely has no horizontal repeatability - this is Bibcheck being unable to distinguish e.g. $5 (money) from double-dagger5 (subfield), e.g. /records/9220/ (test #225)
-			'541: Subfield _. is not allowed.'	// Dollar bug as per other whitelistings, e.g. /records/137359/
+			'541: Subfield _. is not allowed.',	// Dollar bug as per other whitelistings, e.g. /records/137359/
 			'Record is post 1900 but contains local information \(541 or 561 fields\) - please check.',	// For 541; confirmed fine as we are setting $5, e.g. /records/9220/ (test #226)
 			'6XX: Unless the Literary form in the 008 is set to one of the fiction codes, there must be at least one 6XX field \(ignore if the work is a sacred text.\)',		// This arises because Bibcheck has a litcode check at line 602 but that assumes that the 008 is a "008 - Books" which is not always the case - see position_18_34__33 in generate008; see e-mail dated 30/Mar/2016 investigating this, and e-mail 31/Mar/2016 confirming the error is safe to suppress; e.g. /records/1061/ (test #227)
 			'700: Subfield _1 is not allowed.',	// E.g. /records/194888/ has "proposed $12-billion"