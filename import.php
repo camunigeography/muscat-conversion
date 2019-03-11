d14c6032ce46d75e91cce2f97467f3086fe87792
mvl22 <martin@lucas-smith.co.uk>
2019-03-11 12:20:56 +0000

    Revert erroneous deletion in 46fb7cd91ad0275c9a6cf13b8b88a229ad5055a1

--- import.php
+++ import.php
@@ -2412,6 +2412,11 @@ class import
 			}
 		}
 		
+		# Update the status of migrate records to migratewithitem when there are item records specified
+		# This includes the status=migrate constraint to ensure that supressed records don't get unsuppressed, e.g. /records/2096/
+		$query = "UPDATE catalogue_marc SET status = 'migratewithitem' WHERE status = 'migrate' AND itemRecordsIteration2 >= 1;";
+		$this->databaseConnection->execute ($query);
+		
 		# Generate the output files
 		$this->createMarcExports (false, !$isSelection, $errorsHtml /* amended by reference */);
 		
